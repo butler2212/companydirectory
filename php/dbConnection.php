ffc003f7789c91ee29f30c2eab1abce651686e88
Jordan Butler <73473492+butler2212@users.noreply.github.com>
2021-09-28 08:14:35 +0100

    Update dbConnection.php

--- php/dbConnection.php
+++ php/dbConnection.php
@@ -4,10 +4,10 @@
   $user_name = 'dbu2253672';
   $password = '%P4xCP3YH5*vS_a';
 
-  $link = new mysqli($host_name, $user_name, $password, $database);
+  $conn = new mysqli($host_name, $user_name, $password, $database);
 
-  if ($link->connect_error) {
-    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
+  if ($conn->connect_error) {
+    die('<p>Failed to connect to MySQL: '. $conn->connect_error .'</p>');
   } else {
     echo '<p>Connection to MySQL server successfully established.</p>';
   }
