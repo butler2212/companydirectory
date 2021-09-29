7c76a8cdd1bfd883d921b2ab7a342f67cd5bbecd
Jordan Butler <73473492+butler2212@users.noreply.github.com>
2021-09-29 17:17:00 +0100

    Update dbConnection.php

--- php/dbConnection.php
+++ php/dbConnection.php
@@ -8,7 +8,5 @@
 
   if ($conn->connect_error) {
     die('<p>Failed to connect to MySQL: '. $conn->connect_error .'</p>');
-  } else {
-    echo '<p>Connection to MySQL server successfully established.</p>';
-  }
+  } 
 ?>
