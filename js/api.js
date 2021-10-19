3ced9382846a9c41fbc75b600d06f1d3a68153d3
Jordan Butler <73473492+butler2212@users.noreply.github.com>
2021-10-19 20:25:45 +0100

    changes to controls

--- js/api.js
+++ js/api.js
@@ -20,9 +20,9 @@ var sticky = header.offsetTop;
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
+      header.classList.add("sticky", "row");
     } else {
-      header.classList.remove("sticky");
+      header.classList.remove("sticky", "row");
     }
   }
 
